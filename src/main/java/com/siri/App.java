fb0437cc086007791bc78ca84a25f741c4a7d71c
root <root@ip-172-31-4-71.ec2.internal>
2023-05-10 12:41:25 +0000

    first commit

---
+++ src/main/java/com/siri/App.java
@@ -0,0 +1,13 @@
+package com.siri;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
